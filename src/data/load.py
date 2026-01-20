f4f464bfe8f62484fca5ec441468db0b64b490a8
kaan <uepef@student.kit.edu>
2026-01-20 16:35:51 +0000

    feat: Add functions for data mining. docs: add comments for each function.

--- src/data/load.py
+++ src/data/load.py
@@ -1,21 +1,38 @@
 import pandas as pd
 import os
 
-def load_database(index_col=None):
+# Loads the database.
+def _load_database(index_col=None):
     data_path = os.getenv("DATA_PATH")
     return pd.read_csv(data_path, index_col=index_col)
 
+# Checks whether the sommelier present.
+def _is_sommelier_present(sommelier_name: str) -> bool:
+    df = _load_database(index_col="taster_name")
+    return sommelier_name in df.index
+
+# Returns all the columns.
 def get_columns() -> list[str]:
-    df = load_database()
+    df = _load_database()
     return df.columns.to_list()
 
-
+# Returns information about sommelier.
 def get_sommelier_info(sommelier_name: str) -> list[str]:
-    df = load_database(index_col="taster_name")
-    tasters = df.index.to_list()
-    if sommelier_name not in tasters:
-        raise RuntimeError("There is not such a sommelier")
+    df = _load_database(index_col="taster_name")
+    
+    if not _is_sommelier_present(sommelier_name):
+        raise ValueError("There is not such a sommelier.")
     
     return df.loc[sommelier_name].to_list()
 
-    
+# Returns information about each feature.
+def get_feature_info(feature: str) -> list[str]:
+    df = _load_database()
+    return df[feature].to_list()
+
+# Returns top rated wines by a somelier.
+def get_top_rated_wines_by_sommelier(sommelier_name: str, n=10) -> list[int]: 
+    df = _load_database(index_col="taster_name")
+    if not _is_sommelier_present(sommelier_name):
+        raise ValueError("There is not such a sommelier.")
+    return df.loc[sommelier_name, "points"].sort_values().head(10).to_list()
